1def9deb777a1b24d7f7fc26e79b4f46a1a089b7
Logan Saso <logansaso@gmail.com>
2019-11-17 17:01:54 -0800

    Added basic endpoints for each item

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,23 +1,82 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![feature(proc_macro_hygiene, decl_macro, try_trait)]
+
+mod customers;
+mod debug_inserts;
+mod model;
+mod movies;
+mod rooms;
+mod showings;
+mod table_ops;
+mod theater;
+mod tickets;
 
 use mysql;
-
-use rocket;
-use rocket::http::Status;
-use rocket::routes;
-use rocket::{get, post};
+use rocket::{self, get, routes};
 use rocket_contrib::database;
 // use rocket_contrib::json::Json;
 
-mod create_tables;
-use create_tables::*;
+use customers::*;
+use movies::*;
+use rooms::*;
+use showings::*;
+use table_ops::*;
+use theater::*;
+use tickets::*;
 
 #[database("mariadb")]
 pub struct DBConn(mysql::Conn);
 
 fn main() {
     rocket::ignite()
-        .mount("/api", routes![ping, create_tables])
+        .mount("/api", routes![ping, create_tables, drop_tables, insert])
+        .mount(
+            "/api/movies",
+            routes![
+                select_movie_by_id_handler,
+                insert_movie_handler,
+                list_movies_handler
+            ],
+        )
+        .mount(
+            "/api/customers",
+            routes![
+                select_customer_by_id_handler,
+                insert_customer_handler,
+                list_customers_handler
+            ],
+        )
+        .mount(
+            "/api/rooms",
+            routes![
+                select_room_by_id_handler,
+                insert_room_handler,
+                list_rooms_handler
+            ],
+        )
+        .mount(
+            "/api/showings",
+            routes![
+                select_showing_by_id_handler,
+                insert_showing_handler,
+                list_showings_handler
+            ],
+        )
+        .mount(
+            "/api/theaters",
+            routes![
+                select_theater_by_id_handler,
+                insert_theater_handler,
+                list_theaters_handler
+            ],
+        )
+        .mount(
+            "/api/tickets",
+            routes![
+                select_ticket_by_id_handler,
+                insert_ticket_handler,
+                list_tickets_handler
+            ],
+        )
         .attach(DBConn::fairing())
         .launch();
 }
@@ -25,25 +84,4 @@ fn main() {
 #[get("/ping")]
 pub fn ping() -> &'static str {
     "pong"
-}
-
-#[post("/create_tables", format = "application/json")]
-pub fn create_tables(mut conn: DBConn) -> Status {
-    for stmt in [
-        CREATE_CUSTOMERS_QUERY,
-        CREATE_MOVIES_QUERY,
-        CREATE_ROOMS_QUERY,
-        CREATE_SHOWINGS_QUERY,
-        CREATE_THEATERS_QUERY,
-        CREATE_TICKETS_QUERY,
-    ]
-    .iter()
-    {
-        let exe = conn.query(stmt);
-        if exe.is_err() {
-            return Status::new(500, "error creating tables");
-        }
-    }
-
-    Status::new(204, "created")
 }
